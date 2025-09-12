a2b64a174f17fda2d3c63625e6842892ac4f940d
Moses Ramirez <mramirez4@byupathway.edu>
2025-09-11 17:58:36 -1000

    Update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,19 @@
-const lastModifiedSpan = document.getElementById('lastModified');
-if (lastModifiedSpan) {
-  const modified = new Date(document.lastModified);
-  const formatted = `${modified.getMonth() + 1}/${modified.getDate()}/${modified.getFullYear()} ${modified.toLocaleTimeString()}`;
-  lastModifiedSpan.textContent = formatted;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const nav = document.getElementById("mainNav");
+
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+
+  // Set current year in footer
+  document.getElementById("year").textContent = new Date().getFullYear();
+
+  // Set last modified date
+  const lastModifiedSpan = document.getElementById('lastModified');
+  if (lastModifiedSpan) {
+    const modified = new Date(document.lastModified);
+    const formatted = `${modified.getMonth() + 1}/${modified.getDate()}/${modified.getFullYear()} ${modified.toLocaleTimeString()}`;
+    lastModifiedSpan.textContent = formatted;
+  }
+});
